4fc7a3b294443a49709a332a20c3ced53bce12ee
Sasa Tatar <sasa.tatar@gmail.com>
2017-05-04 12:21:37 +0200

    Fix Store examples

--- app/sections/examples/store.tsx
+++ app/sections/examples/store.tsx
@@ -31,39 +31,20 @@ import {LabelsLeftLayout, LabelsTopLayout, Controller} from "cx/ui";
 import { Chart, Gridlines, Legend, LineGraph, NumericAxis } from "cx/charts";
 import { Rectangle, Svg } from "cx/svg";
 
-function loadCulture(culture) {
-    //code-splitting - it's mandatory to use string constants so webpack can know how to prepare packages
-    switch (culture) {
-        case 'de-de':
-            return System.import('cx/locale/de-de');
-
-        default:
-        case 'en-us':
-            return System.import('cx/locale/en-us');
-    }
-}
-
-function setCulture(culture, store) {
-    loadCulture(culture)
-        .then(() => {
-            Culture.setCulture(culture);
-            store.notify();//force re-render
-        });
-}
-
 class PageController extends Controller {
-    init() {
-        super.init();
-                
-        this.store.init('$page.number', 123456.78);
-        this.store.init('$page.date', new Date().toISOString());
-        this.store.set("intro.core.items", [
-          { text: "A", checked: false },
-          { text: "B", checked: false },
-          { text: "C", checked: false }
-        ]);
-        this.store.set('intro.core.letterCount', '');
+    onInit() {
+        this.store.init('$page', {
+            name: 'Jane',
+            disabled: true,
+            todoList: [
+                { id: 1, text: 'Learn Cx', done: true }, 
+                { id: 2, text: "Feed the cat", done: false },
+                { id: 3, text: "Take a break", done: false }
+            ],
+            count: 0
+        });
     }
+
     greet() {
         let name = this.store.get('$page.name')
         MsgBox.alert(`Hello, ${name}!`);
@@ -73,7 +54,6 @@ class PageController extends Controller {
 register('Store', 'Other examples', <cx>
     <h2 putInto="header">Store</h2>
     <div class="example pad wrap">
-        <h3>Store</h3>
         <FlexRow wrap spacing="large">
             <Section mod="well" header={{ level: 5, text: 'Counter'}} layout={{type: LabelsTopLayout, vertical: true}}>
                 <div layout={LabelsTopLayout} controller={PageController}>
@@ -91,12 +71,7 @@ register('Store', 'Other examples', <cx>
                   </Button>
                 </div>
             </Section>
-            <Section mod="well">
-                <Checkbox value={{ bind: 'intro.core.checked' }}>Checkbox</Checkbox>
-                <TextField value={{ bind: 'intro.core.text' }}
-                      enabled={{ expr: '!{intro.core.checked}' }}/>
-            </Section>
-            <Section mod="well">
+            <Section mod="well" header={{ level: 5, text: 'store.delete()'}}>
                 <div layout={LabelsTopLayout}>
                     <TextField value={{ bind: "$page.name" }} label="Name" />
                     <Button onClick={(e, {store}) =>
@@ -106,7 +81,7 @@ register('Store', 'Other examples', <cx>
                     </Button>
                 </div>
             </Section>
-            <Section mod="well">
+            <Section mod="well" header={{ level: 5, text: 'set + get'}}>
                 <div layout={LabelsTopLayout} >
                     <TextField label="Name" value={{ bind: "$page.name" }} disabled={{ bind: "$page.disabled" }} />
                     <Button onClick={(e, instance) => {
@@ -117,7 +92,18 @@ register('Store', 'Other examples', <cx>
                     />
                 </div>
             </Section>
-            <Section mod="well">
+            <Section mod="well" header={{ level: 5, text: 'store.toggle()'}}>
+                <div layout={LabelsTopLayout} >
+                    <TextField label="Name" value={{ bind: "$page.name" }} disabled={{ bind: "$page.disabled" }} />
+                    <Button onClick={(e, instance) => {
+                            let {store} = instance;
+                            store.toggle("$page.disabled");
+                        }}
+                        text={computable('$page.disabled', (disabled) => disabled ? "Enable input" : "Disable input")}   
+                    />
+                </div>
+            </Section>
+            <Section mod="well" header={{ level: 5, text: 'store.copy()'}}>
                 <div layout={LabelsTopLayout}>
                     <TextField label="Origin" value={{ bind: "$page.name" }}/>
                     <TextField label="Destination" value={{ bind:"$page.copyDestination" }} placeholder="click Copy" />
@@ -126,37 +112,7 @@ register('Store', 'Other examples', <cx>
                     }}>Copy</Button>
                 </div>
             </Section>
-            <Section mod="well">
-                <div>
-                    <div preserveWhitespace>
-                        <Radio value={{bind: "$page.place", defaultValue: "winner"}} option="winner">Winner</Radio>
-                        <Radio value={{ bind: "$page.place" }} option="second">2nd Place</Radio>
-                        <Radio value={{ bind: "$page.place" }} option="third">3rd Place</Radio>
-                    </div>
-                    <hr/>
-                    <Sandbox key={{ bind: "$page.place" }} storage={{ bind: "$page.results" }} recordName="$contestant">
-                        <div layout={LabelsLeftLayout}>
-                            <TextField value={{ bind: "$contestant.firstName" }} label="First Name"/>
-                            <TextField value={{ bind: "$contestant.lastName" }} label="Last Name"/>
-                        </div>
-                    </Sandbox>
-                </div>
-                <div style="width:200px">
-                    <strong>Results</strong>
-                    <Rescope bind="$page.results">
-                        <div>
-                            1. <Text tpl="{winner.firstName} {winner.lastName}" />
-                        </div>
-                        <div>
-                            2. <Text tpl="{second.firstName} {second.lastName}" />
-                        </div>
-                        <div>
-                            3. <Text tpl="{third.firstName} {third.lastName}" />
-                        </div>
-                    </Rescope>
-                </div>
-            </Section>
-            <Section mod="well">
+             <Section mod="well" header={{ level: 5, text: 'store.move()'}}>
                 <div layout={LabelsTopLayout}>
                     <TextField label="Origin" value={{ bind: "$page.name" }}/>
                     <TextField label="Destination" value={{ bind: "$page.moveDestination" }} placeholder="click Move" />
@@ -190,121 +146,6 @@ register('Store', 'Other examples', <cx>
                         </Button>
                     </div>
                 </div>
-            </Section>
-            <Section mod="well">
-                 <div layout={LabelsLeftLayout}>
-                    <TextField value={{ bind: 'intro.core.firstName' }} label="First Name" />
-                    <TextField value={{ bind: 'intro.core.lastName' }} label="Last Name"/>
-                    <TextField value={{ tpl: 'Hello {intro.core.firstName} {intro.core.lastName}!' }} label="Template" mode="view"/>
-                    <TextField value={{ expr: '"Hello "+{intro.core.firstName:s}+" "+{intro.core.lastName:s}+"!"' }} label="Expression" mode="view"/>
-                </div>
-            </Section>
-            <Section mod="well">
-                <div preserveWhitespace>
-                    <NumberField value={{ bind: 'intro.core.a' }} placeholder="A" />
-                    +
-                    <NumberField value={{ bind:'intro.core.b' }} placeholder="B" />
-                    =
-                    <Text value={computable('intro.core.a', 'intro.core.b', (a, b) => a==null || b==null ? "ERR" : a + b )} />
-                 </div>
-            </Section>
-            <Section mod="well">
-                <div preserveWhitespace>
-                    A + 2 = <NumberField style="width:50px"
-                                        value={{
-                                            expr: '{intro.core.a}+2',
-                                            set: (value, {store}) => { store.set('intro.core.a', value - 2) }
-                                        }}/>
-                    <br/>
-                    A = <Text bind="intro.core.a" />
-                </div>
-            </Section>
-          
-            <Section mod="well" title="Direct">
-                <Slider value={{ bind: "$page.slider.direct" }}/>
-                <Slider value={{ bind: "$page.slider.direct" }}/>
-            </Section>
-            <Section mod="well" title="Throttle: 300ms">
-                <Slider value={{ bind: "$page.slider.throttled" , throttle: 300  }}/>
-                <br />
-                <Slider value={{ bind: "$page.slider.throttled", throttle: 300 }} />
-            </Section>
-            <Section mod="well" title="Debounce: 300ms">
-                <Slider value={{ bind: "$page.slider.debounced", debounce: 300 }} />
-                <br />
-                <Slider value={{ bind: "$page.slider.debounced", debounce: 300 }} />
-            </Section>
-
-           
-        </FlexRow>
-        <h3 style={{marginTop: "3rem"}}>Standalone</h3>
-
-        <FlexRow wrap spacing="large">
-
-            <Section mod="well" header={{ level: 4, text: "Calendar"}} layout={{type: LabelsTopLayout, vertical: true}}>
-                <Calendar value={{ bind: "date" }}/>
-            </Section>
-
-            <Section mod="well" header={{ level: 4, text: "MonthPicker"}}
-                     layout={{type: LabelsTopLayout, vertical: true}}>
-                <MonthPicker
-                    range
-                    from={{ bind: "dateFrom" }}
-                    to={{ bind: "dateTo" }}
-                    style="height:21rem"
-                />
-            </Section>
-            <Section mod="well">
-                <div class="widgets" controller={PageController}>
-                    <div preserveWhitespace>
-                        <Button onClick={(e, {store}) => {setCulture('de-de', store)}}>de-de</Button>
-                        <Button onClick={(e, {store}) => {setCulture('en-us', store)}}>en-us</Button>
-                    </div>
-                    <div layout={LabelsLeftLayout}>
-                        <NumberField value={{ bind: "$page.number" }} required />
-                        <DateField value={{ bind: "$page.date" }} required />
-                        <NumberField value={{ bind: "$page.number" }} required format="currency"/>
-                        <Calendar value={{ bind: "$page.date" }}/>
-                    </div>
-                </div>
-            </Section>
-            <Section mod="well" header={{ level: 4, text: "ColorPicker"}}
-                layout={{type: LabelsTopLayout, vertical: true}}>
-                <ColorPicker value={{ bind: "color" }}/>
-            </Section>
-            <Section mod="well">
-                <Svg style="width:300px;height:200px" margin="10 20 30 50">
-                    <Chart axes={{
-                        x: <NumericAxis />,
-                        y: <NumericAxis vertical/>
-                    }}>
-                        <Rectangle fill="white" />
-                        <Gridlines />
-                        <LineGraph name="Line 1"
-                                    colorIndex={5}
-                                    data={[{x: 0, y: 0}, {x: 100, y: 100}, {x: 200, y: 150}]} />
-
-                        <LineGraph name="Line 2"
-                                    colorIndex={10}
-                                    data={[{x: 0, y: 50}, {x: 100, y: 150}, {x: 200, y: 100}]} />
-                    </Chart>
-                </Svg>
-                <Legend vertical />
-            </Section>
-            <Section mod="well">
-                <Repeater records={{ bind: "intro.core.items" }}>
-                    <Checkbox value={{ bind: "$record.checked" }} text={{ bind: "$record.text" }}/>
-                    <br/>
-                </Repeater>
-
-                You checked <Text value={{ expr: '{intro.core.items}.filter(a=>a.checked).length' }}/> item(s).
-            </Section>
-            <Section mod="well">
-                <TextField value={{ bind: 'intro.core.letterCount' }} placeholder="Type here" />
-                <p>
-                <Text value={(storeData) => `You typed letter A
-                ${((storeData.intro.core.letterCount || '').match(/A/g) || []).length} times.` } />
-                </p>
             </Section>
         </FlexRow>
     </div>
